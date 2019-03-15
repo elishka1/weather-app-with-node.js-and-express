fd0c179adc4a143df54c2b08c82fa0cdecd09dd6
Code Nation 22 <codenation@CN-22.lan>
2019-03-15 17:25:56 +0000

    fff

--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
 console.log("client side JavaScript has loaded")
+
+
+let go1 = document.getElementById('go');
+let box = document.getElementById('box');
+let submit1 = document.querySelector('form');
+let p = document.getElementById('p');
+console.log(submit1)
+submit1.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+let address = box.value;
+
+    fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+    response.json().then((data) => {
+         p.textContent=data.forecast
+    })
+    });
+})
--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
 const path = require('path');
 const express = require('express');
+const geocode = require('./geocode');
+const forecast = require('./forecast')
 
 
 const app = express();
 
+
 const publicDirectory = path.join(__dirname, '../public')
 app.use(express.static(publicDirectory))
 
-// app.get('', function (req, res) {
-//     res.send('Hiya')
-// })
+app.get('/weather', function (req, res) {
+    if (!req.query.address) {
+        res.send('Please pick an address')
+    } else {
+        geocode(req.query.address, (error, response ) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            forecast(response.latitude, response.longitude, (error, forecastData) => {
+                if (error) {
+                    return console.log(error)
+                }
+                
+                res.send({forecast: forecastData,
+                    location: response.location,
+                    address: req.query.address
+                })
+              
+            })
+        })
+    }
+    
+})
 
 
 
