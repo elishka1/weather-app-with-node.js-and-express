18df85a703633ba60b176aa859672db2150e6355
Code Nation 22 <codenation@CN-22.lan>
2019-03-19 11:10:04 +0000

    weather app updated

--- public/index.js
+++ public/index.js
@@ -1,19 +1,34 @@
+
+
 console.log("client side JavaScript has loaded")
 
 
 let go1 = document.getElementById('go');
 let box = document.getElementById('box');
-let submit1 = document.querySelector('form');
-let p = document.getElementById('p');
-console.log(submit1)
-submit1.addEventListener('submit', (e) => {
+
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2')
+
+
+
+go1.addEventListener('click', (e) => {
     e.preventDefault();
 
 let address = box.value;
 
+p1.textContent = "Loading..."
+p2.textContent = "";
+
+
     fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
     response.json().then((data) => {
-         p.textContent=data.forecast
+
+        if (data.error) {
+            p1.textContent = data.error
+        } else {
+            p1.textContent=data.location
+            p2.textContent = data.forecast
+        }
     })
     });
 })
